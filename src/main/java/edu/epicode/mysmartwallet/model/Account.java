c62040154f5f5cb5db516ce0472d9d876aaf480e
Jesse Galetta <jesse.galetta@gmail.com>
2026-01-14 19:17:43 +0100

    Implementata classe Account

---
+++ src/main/java/edu/epicode/mysmartwallet/model/Account.java
@@ -0,0 +1,137 @@
+package edu.epicode.mysmartwallet.model;
+
+import edu.epicode.mysmartwallet.util.AppLogger;
+import edu.epicode.mysmartwallet.util.MoneyUtil;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * Rappresenta un conto finanziario con le sue transazioni.
+ * Il saldo viene calcolato dinamicamente dalla somma delle transazioni.
+ *
+ * @author Jesse Galetta
+ * @version 1.0
+ */
+public class Account extends BaseEntity {
+
+    private static final Logger logger = AppLogger.getLogger(Account.class);
+
+    private final String name;
+    private final int currencyId;
+    private final BigDecimal initialBalance;
+    private final List<Transaction> transactions;
+
+    /**
+     * Crea un nuovo conto.
+     *
+     * @param id             l'identificativo univoco
+     * @param name           il nome del conto
+     * @param currencyId     l'ID della valuta del conto
+     * @param initialBalance il saldo iniziale
+     */
+    public Account(int id, String name, int currencyId, BigDecimal initialBalance) {
+        super(id);
+        this.name = name;
+        this.currencyId = currencyId;
+        this.initialBalance = initialBalance;
+        this.transactions = new ArrayList<>();
+        logger.fine("Creato conto: " + name + " con saldo iniziale " + initialBalance);
+    }
+
+    /**
+     * Aggiunge una transazione al conto.
+     *
+     * @param transaction la transazione da aggiungere
+     */
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+        logger.fine("Aggiunta transazione " + transaction.getId() + " al conto " + name);
+    }
+
+    /**
+     * Rimuove una transazione dal conto.
+     *
+     * @param transactionId l'ID della transazione da rimuovere
+     * @return true se la transazione è stata rimossa, false altrimenti
+     */
+    public boolean removeTransaction(int transactionId) {
+        boolean removed = transactions.removeIf(t -> t.getId() == transactionId);
+        if (removed) {
+            logger.fine("Rimossa transazione " + transactionId + " dal conto " + name);
+        }
+        return removed;
+    }
+
+    /**
+     * Calcola e restituisce il saldo attuale del conto.
+     * Il saldo è calcolato come: saldo iniziale + entrate - uscite - trasferimenti.
+     *
+     * @return il saldo corrente
+     */
+    public BigDecimal getBalance() {
+        BigDecimal balance = initialBalance;
+
+        for (Transaction t : transactions) {
+            switch (t.getType()) {
+                case INCOME:
+                    balance = MoneyUtil.add(balance, t.getAmount());
+                    break;
+                case EXPENSE:
+                case TRANSFER:
+                    balance = MoneyUtil.subtract(balance, t.getAmount());
+                    break;
+            }
+        }
+
+        return balance;
+    }
+
+    /**
+     * Restituisce una copia difensiva della lista delle transazioni.
+     *
+     * @return lista delle transazioni
+     */
+    public List<Transaction> getTransactions() {
+        return new ArrayList<>(transactions);
+    }
+
+    /**
+     * Restituisce il nome del conto.
+     *
+     * @return il nome
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Restituisce l'ID della valuta del conto.
+     *
+     * @return l'ID della valuta
+     */
+    public int getCurrencyId() {
+        return currencyId;
+    }
+
+    /**
+     * Restituisce il saldo iniziale del conto.
+     *
+     * @return il saldo iniziale
+     */
+    public BigDecimal getInitialBalance() {
+        return initialBalance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", currencyId=" + currencyId +
+                ", balance=" + getBalance() +
+                '}';
+    }
+}
